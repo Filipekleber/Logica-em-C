6f30e4e8eaf692ba653f5270f14fc49a92e01c16
FilipeKleber <filiperrggvaca@gmail.com>
2025-03-04 16:16:00 -0300

    Mudando algumas funções e adicionando a função "switch" e um nivel para o jogador escolher.

--- Jogo.c
+++ Jogo.c
@@ -16,9 +16,9 @@ int queroJogar(){
 //Bloco aonde vai extrair a resposta do jogador
 
     if(resposta == 'sim' || resposta == 'Sim'){
-        return 1;
-    }else{
         return 0;
+    }else{
+        return 1;
     }
 }
 //------------------------------------------------------------------------------------------------------------
@@ -29,20 +29,50 @@ int main(){
         printf("Jogo encerrado...\n");
         return 0;
     }
-
-    printf("Vamos comecar o jogo!!\n\n\n");
-
-    int segundos = time(0);
-    srand(segundos);
-    int numeroGrande = rand();
-
+//------------------------------------------------------------------------------------------------------------
+    
     int numeroSecreto = numeroSecreto % 100; 
     int chuteJogador;
     int tentativas = 1;
     double pontos = 1000;
+    
+    int acertou = 0;
 
-    while (1)
-    {
+    printf("Vamos comecar o jogo!!\n\n\n");
+//----------------------------------------------------------------------------------------------------------------
+
+    //Fução que ira perguntar ao jogador qual o nivel que ele quer jogar.
+    int nivel;
+    printf("Qual nivel voce vai querer jogar?");
+    printf("(1) facil (2) medio (3) Dificil:    ");
+    scanf("%d", &nivel);
+
+    int numeroTentativas;
+
+switch (nivel)
+{
+    case 1: numeroTentativas = 20;
+    break;
+
+    case 2: numeroTentativas = 10;
+    break;
+
+    default: numeroTentativas = 6;
+    break;
+}
+
+//----------------------------------------------------------------------------------------------------------------
+
+//Função para gerar um número aleatorio.
+    int segundos = time(0);
+    srand(segundos);
+    int numeroGrande = rand();
+
+  
+
+ 
+    for (int i = 1; i <= numeroTentativas; i++){
+        
         printf("Tentativas %d\n", tentativas);
         printf("Escolha um numero para comecar: ");
         scanf(" %d", &chuteJogador);
@@ -55,9 +85,9 @@ int main(){
 
         int acertou = chuteJogador == numeroSecreto;
 
-        if(acertou){
-            printf("Parabens, Você acertou!");
+        if(acertou){    
             break;
+        
         }else{
             int maior = (chuteJogador > numeroSecreto);
 
@@ -76,9 +106,16 @@ int main(){
         pontos = pontos - pontosPerdidos;
 }
 
-printf("O numero de tentativas foi: %d\n", tentativas);
-printf("Sua pontuação foi de: %.1f\n", pontos);
 printf("Fim de jogo\n");
+if (acertou){
+    printf("Parabens, Você acertou!\n");
+    printf("O numero de tentativas foi: %d\n", tentativas);
+    printf("Sua pontuação foi de: %.1f\n", pontos);
+}else{
+    printf("Você perdeu, tente novamente\n");
+}
+
+
 
 return 0;
 
